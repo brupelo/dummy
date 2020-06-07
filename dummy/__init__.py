0d1c39c6b9ce9a3f4f5e965afef5d39022375f5d
BPL <bpl@whatever.com>
2020-06-07 17:56:55 +0200

    Fix broken code

---
+++ dummy/__init__.py
@@ -0,0 +1,6 @@
+from dummy.domain1.config import Config as ConfigDomain2
+
+__all__ = [
+    "ConfigDomain1",
+    "ConfigDomain2",
+]
--- dummy/app.py
+++ dummy/app.py
@@ -1,13 +1,17 @@
-from pathlib import Path
-
 from flask import Flask
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def create_app(name, config):
-    app = Flask(name)
+
+def create_app(domain_name):
+    # TODO: Should follow OCP eventually, let's keep it simple for now though
+    from dummy import ConfigDomain1, ConfigDomain2
+
+    config = {"domain1": ConfigDomain1, "domain2": ConfigDomain2}[domain_name]()
+
+    app = Flask()
     app.config.update(config.db_config)
     db.init_app(app)
     return app
